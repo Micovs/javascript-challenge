428b27bc64d576c95a46e4ad28b8df9dc3be0655
micovs <stojancomicov@gmail.com>
2020-10-12 01:05:42 -0400

    Part 2 completed

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -18,8 +18,8 @@ tableData.forEach((report) => {
  // Select the button
 var button = d3.select("#filter-btn");
 
-// Select the form
-var form = d3.select(".form-control"); 
+// Select the form --- changed from part one, explanation on line 32
+var form = d3.select("#shape"); 
 
 // Create event handlers
 // **********
@@ -27,6 +27,9 @@ var form = d3.select(".form-control");
 button.on("click", runEnter);
 
 // Second event when space or Enter is pressed
+// ******************************
+// For this part the event handler is set up for the last cell, which is the shape.
+// The idea is to activate the search only after the user is finished entering the last field, which is the shape.
 form.on("keypress", function() {
     if(d3.event.keyCode === 32 || d3.event.keyCode === 13){
     runEnter();
@@ -39,19 +42,33 @@ function runEnter() {
     // Prevent the page from refreshing
     d3.event.preventDefault();
     
-    // Select the input element and get the raw HTML node
+    // Select the input elements and get the raw HTML node
     var inputElement = d3.select("#datetime");
-  
+    var inputElementCity = d3.select("#city");
+    var inputElementState = d3.select("#state");
+    var inputElementCountry = d3.select("#country");
+    var inputElementShape = d3.select("#shape");
+
+
     // Get the value property of the input element
     var inputValue = inputElement.property("value");
-    console.log(inputValue)
-  
+    var inputValueCity = inputElementCity.property("value");
+    var inputValueState = inputElementState.property("value");
+    var inputValueCountry = inputElementCountry.property("value");
+    var inputValueShape = inputElementShape.property("value");
+
+    
     // Sets the value that was inputed by the user as a placeholder
-    inputElement.attr("placeholder", inputValue)
+    inputElement.attr("placeholder", inputValue);
+    inputElementCity.attr("placeholder", inputValueCity);
+    inputElementState.attr("placeholder", inputValueState);
+    inputElementCountry.attr("placeholder", inputValueCountry);
+    inputElementShape.attr("placeholder", inputValueShape);
 
 
     // Filters the data based on the user input
-    var filteredData = tableData.filter(report => report.datetime === inputValue);
+    var filteredData = tableData.filter(report => (report.datetime === inputValue && report.city === inputValueCity &&
+      report.state === inputValueState && report.country === inputValueCountry && report.shape === inputValueShape));
 
     // Clean the tbody so the new filtered data can be shown
     tbody.html("");
