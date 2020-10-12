bd8d760cdd832740112b2f6cc27c5087c85db385
micovs <stojancomicov@gmail.com>
2020-10-11 22:07:02 -0400

    table created

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,13 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+tableData.forEach((report) => {
+    var row = tbody.append("tr");
+    Object.entries(report).forEach(([key, value]) => {
+      var cell = row.append("td");
+      cell.text(value);
+    });
+  });
